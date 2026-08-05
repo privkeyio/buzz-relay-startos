95cdf5db9cb6275773091dc57e2e6cb9739ca2f4
William K. Santiago <william@privkey.io>
2026-08-05 16:42:49 -0400

    Add diagnostics action surfacing community connection state

--- startos/actions/index.ts
+++ startos/actions/index.ts
@@ -1,4 +1,5 @@
 import { sdk } from '../sdk'
 import { config } from './config'
+import { diagnostics } from './diagnostics'
 
-export const actions = sdk.Actions.of().addAction(config)
+export const actions = sdk.Actions.of().addAction(config).addAction(diagnostics)
